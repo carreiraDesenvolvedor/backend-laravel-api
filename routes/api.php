34e23d2db40c097233ffcad804340dbb172e9f66
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-05-19 12:27:31 +0100

    Initiating laravel project + JWT auth

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::controller(AuthController::class)->prefix('auth')->group(function () {
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+});
